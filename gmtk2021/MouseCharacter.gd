e36809d53d76618f9c986c5e34e0cc971d0ceaed
Ismail Amer <imav97@hotmail.com>
2021-06-11 16:51:37 -0400

    after crash commit

--- gmtk2021/MouseCharacter.gd
+++ gmtk2021/MouseCharacter.gd
@@ -1,25 +1,32 @@
 extends KinematicBody2D
 
 #movement variables
-export var speed := 50
+export var speed :=  25
 var velocity := Vector2.ZERO
+var mouse_position := Vector2.ZERO
 
 
-func get_input():
-	if Input.is_action_just_pressed("left_click"):
-		
-		if self.position.x > event.position.x:
-			velocity.x = -1
-		else:
-			velocity.x = 1
-		if self.position.y > event.position.y:
-			velocity.y = -1
-		else:
-			velocity.y = 1
+func get_movement():
+	if self.position.x > mouse_position.x:
+		velocity.x = -1
+		print(self.position.x)
+	else:
+		velocity.x = 1
+	
+	if self.position.y > mouse_position.y:
+		velocity.y = -1
+	else:
+		velocity.y = 1
 			
 		velocity = velocity * speed
 
 
+func _input(event):
+	if event is InputEventMouseButton:
+		mouse_position = get_viewport().get_mouse_position()
+		print(mouse_position)
+
 
 func _physics_process(delta):
+	get_movement()
 	velocity = move_and_slide(velocity)
