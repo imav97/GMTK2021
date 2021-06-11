a01ab2c96d1298aa95a5bed02858362401345289
Ismail Amer <imav97@hotmail.com>
2021-06-11 18:21:25 -0400

    ADDed swipe detection

--- gmtk2021/MouseCharacter.gd
+++ gmtk2021/MouseCharacter.gd
@@ -8,13 +8,26 @@ var moving := false
 var destination := Vector2.ZERO
 var movement := Vector2.ZERO
 
+var initial_mouse_position := Vector2.ZERO
+var final_mouse_position := Vector2.ZERO
+
 
 
 func _unhandled_input(event):
-	if event.is_action_pressed("left_click"):
+	if event.is_action_pressed("right_click"):
 		moving=true
 		destination = get_global_mouse_position()
-
+	
+	if event.is_action_pressed("left_click"):
+		initial_mouse_position = event.position
+	if event.is_action_released("left_click"):
+		final_mouse_position = event.position
+		
+		#choosing whether it was a horizontal or a vertical swipe
+		if abs(final_mouse_position.x - initial_mouse_position.x) > abs(final_mouse_position.y - initial_mouse_position.y):
+			print("swipe was horizontal ")
+		else:
+			print("swipe was vertical ")
 
 func _physics_process(delta):
 	movement_loop(delta)
