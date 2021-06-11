02ed71defe7cfe8bb396f7f41da02cc289db03cc
Ismail Amer <imav97@hotmail.com>
2021-06-11 16:08:56 -0400

    keyboard attack raycast added

--- gmtk2021/LeftCharacter.gd
+++ gmtk2021/LeftCharacter.gd
@@ -1,22 +1,45 @@
 extends KinematicBody2D
 
-export var speed = 350
+#movement variables
+export var speed := 350
+var velocity := Vector2.ZERO
 
-var velocity = Vector2.ZERO
+# Attack variables
+export var attack_cooldown_time := 1000
+export var attack_damage := 30
+var next_attack_time := 0
+var attack_direction := Vector2.ZERO
+
+
 
 func get_input():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed('right'):
 		velocity.x += 1
+		attack_direction.x += 1
 	if Input.is_action_pressed('left'):
 		velocity.x -= 1
+		attack_direction.x -= 1
 	if Input.is_action_pressed('down'):
 		velocity.y += 1
+		attack_direction.y -= 1
 	if Input.is_action_pressed('up'):
 		velocity.y -= 1
-
+		attack_direction.y += 1
+	
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+	if attack_direction != Vector2.ZERO:
+		$RayCast2D.cast_to = attack_direction.normalized() * 50
+
+
+func _input(event):
+	if event.is_action_pressed("attack"):
+	# Check if player can attack
+		var now = OS.get_ticks_msec()
+		if now >= next_attack_time:
+			# Add cooldown time to current time
+			next_attack_time = now + attack_cooldown_time
