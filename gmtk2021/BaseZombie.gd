c43c6045dafb108f27a46d7897e3d5503702cac0
Ismail Amer <imav97@hotmail.com>
2021-06-11 21:55:39 -0400

    committing before sleeping

--- gmtk2021/BaseZombie.gd
+++ gmtk2021/BaseZombie.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
+var player_pos := Vector2.ZERO
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+
 
 
