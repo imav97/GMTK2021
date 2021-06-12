798eb71935652c3de4dc98824c3b85e866f88204
Jose T. Monagas <jmonagas@localhost.localdomain>
2021-06-12 19:47:44 -0400

    Add damage to projectile

--- gmtk2021/Projectile.gd
+++ gmtk2021/Projectile.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 const ACCELERATION: int = 1200
 
 var direction: Vector2 = Vector2.ZERO
-var offset: int = 200
+var offset: int = 100
 var damage: int = 5
 
 
@@ -16,7 +16,6 @@ func init(direction: Vector2, initial_position: Vector2, damage: int = 5) -> voi
 func _physics_process(delta: float) -> void:
 	var collision = move_and_collide(direction * ACCELERATION * delta)
 	if collision:
-		# TODO: Do dmg, damage value comes from self.damage, which can be
-		# passed in the init()
-		print("collides")
-		queue_free()
+		if collision.collider.has_method("take_damage"):
+			collision.collider.take_damage(self.damage)
+			queue_free()
