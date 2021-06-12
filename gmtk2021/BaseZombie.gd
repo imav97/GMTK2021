2c4ee912e8ceabb018ad889a2d395decc52fd690
Jose T. Monagas <jmonagas@localhost.localdomain>
2021-06-12 00:55:33 -0400

    Add Enemy prototype.

--- gmtk2021/BaseZombie.gd
+++ gmtk2021/BaseZombie.gd
@@ -1,9 +1,23 @@
 extends KinematicBody2D
 
-var player_pos := Vector2.ZERO
+const ACCELERATION: int = 1200
+
+var target: KinematicBody2D
+var is_chasing: bool = false
+
+func init(character: KinematicBody2D) -> void:
+	target = character
+
+func _physics_process(delta: float) -> void:
+	if is_chasing:
+		var direction: Vector2 = self.position.direction_to(target.position)
+		var speed: Vector2 = direction * ACCELERATION
+		move_and_slide(speed, Vector2.UP)
 
 
+func _on_PlayerDetection_body_entered(body: PhysicsBody2D) -> void:
+	is_chasing = true
 
 
-
-
+func _on_PlayerDetection_body_exited(body: PhysicsBody2D) -> void:
+	is_chasing = false
