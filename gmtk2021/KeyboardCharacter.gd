8c034d5bddf9848cddd348f6e694bf79f347ca3b
Ismail Amer <imav97@hotmail.com>
2021-06-12 06:33:19 -0400

    added saladin idle animation

--- gmtk2021/KeyboardCharacter.gd
+++ gmtk2021/KeyboardCharacter.gd
@@ -5,7 +5,7 @@ export var speed := 350
 var velocity := Vector2.ZERO
 
 # Attack variables
-export var attack_cooldown_time := 1000
+export var attack_cooldown_time := 400
 export var attack_damage := 30
 var next_attack_time := 0
 
@@ -35,20 +35,20 @@ func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
 	if velocity != Vector2.ZERO:
-		$RayCast2D.cast_to = velocity.normalized() * 10
+		$RayCast2D.cast_to = velocity.normalized() * 18
 		ani_player.play("walk")
-	else:
+	elif !ani_player.is_playing():
 		ani_player.stop()
 
 
 func _input(event):
 	if event.is_action_pressed("simple_attack"):
-	# Check if player can attack
+		ani_player.play("attack")
+		# Check if player can attack
 		var now = OS.get_ticks_msec()
 		if now >= next_attack_time:
 			# Add cooldown time to current time
 			next_attack_time = now + attack_cooldown_time
-			print("melee attack")
 			var enemy = $RayCast2D.get_collider()
 			if enemy != null:
 				if enemy.name.find("BaseZombie") >= 0:
