23d3ca7abe70003f85d8030a81236ac72ad852f5
Ismail Amer <imav97@hotmail.com>
2021-06-12 15:57:25 -0400

    comit after error

--- gmtk2021/TEST_SCENE.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-onready var zombie := $BaseZombie
-onready var player := $KeyboardMovement
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	zombie.player = player
-
