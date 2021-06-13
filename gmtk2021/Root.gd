965362b148d39745c8eddeebf6fb26f1bd706bdb
Ismail Amer <imav97@hotmail.com>
2021-06-13 01:33:43 -0400

    ADDed Credits Scene and changed root scene, better main menu

---
+++ gmtk2021/Root.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("")#sorry m8 no se cual es el main
+	pass # Replace with function body.
+
+
+func _on_Credits_pressed():
+	get_tree().change_scene("res://Addons/godot-credits-master/GodotCredits.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
