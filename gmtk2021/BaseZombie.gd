b87bbec71cada3b00b8627124973f1ccaf6c6638
Jose T. Monagas <jmonagas@localhost.localdomain>
2021-06-12 21:03:53 -0400

    Fix Zombies not chasing character on maps

--- gmtk2021/BaseZombie.gd
+++ gmtk2021/BaseZombie.gd
@@ -1,45 +1,31 @@
 extends KinematicBody2D
 
 const ACCELERATION: int = 5
-const DETECTION_RANGE: int = 60000
+const DETECTION_RANGE: int = 100000
+const ATTACK_RANGE: int = 30000
+
+export var damage: int = 10
 
 var player: KinematicBody2D
-var is_chasing: bool = false
-
-var velocity := Vector2.ZERO
 var speed := 100
-
 var health := 100
 
-
-
-#func init(character: KinematicBody2D) -> void:
-#	player = character
+func _ready():
+	player = get_parent().get_node("Character")
 
 func _physics_process(delta: float) -> void:
 	if !player == null:
+		if self.position.distance_squared_to(player.global_position) < ATTACK_RANGE:
+			player.take_damage(self.damage)
+		
 		if self.position.distance_squared_to(player.position) < DETECTION_RANGE:
-			var player_direction = (player.position - self.position).normalized()
+			var player_direction = (player.global_position - self.global_position).normalized()
 			move_and_slide(speed * player_direction)
-		
-		
-	if health <= 0:
-		get_tree().queue_delete(self)
 
 
 func take_damage(damage: int) -> void:
 	health -= damage
-	print('melee hit registered')
-
-
-func get_movement_direction():
-	if self.position.x < player.position.x:
-		velocity.x += 1
-	else:
-		velocity.x -= 1
-		
-	if self.position.y < player.position.y:
-		velocity.y += 1
-	else:
-		velocity.y -= 1
+	# TODO: Add VFX
+	if health <= 0:
+		queue_free()
 	
